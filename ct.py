d830e94729fe22c6577daba32be6d34f3b92b48d
codewithaysha <ayshathasna.in@gmail.com>
2026-04-29 11:46:28 +0530

    myproject

---
+++ ct.py
@@ -0,0 +1,54 @@
+
+contacts = {}
+
+def add_contact():
+    name = input("Enter name: ")
+    phone = input("Enter phone number: ")
+    contacts[name] = phone
+    print("Contact added successfully!")
+
+def view_contacts():
+    if not contacts:
+        print("No contacts found.")
+    else:
+        for name, phone in contacts.items():
+            print(name, ":", phone)
+
+def search_contact():
+    name = input("Enter name to search: ")
+    if name in contacts:
+        print("Found:", name, "-", contacts[name])
+    else:
+        print("Contact not found.")
+
+def delete_contact():
+    name = input("Enter name to delete: ")
+    if name in contacts:
+        del contacts[name]
+        print("Deleted successfully.")
+    else:
+        print("Contact not found.")
+
+while True:
+    print("\n--- Contact Book ---")
+    print("1. Add Contact")
+    print("2. View Contacts")
+    print("3. Search Contact")
+    print("4. Delete Contact")
+    print("5. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == '1':
+        add_contact()
+    elif choice == '2':
+        view_contacts()
+    elif choice == '3':
+        search_contact()
+    elif choice == '4':
+        delete_contact()
+    elif choice == '5':
+        print("Exiting...")
+        break
+    else:
+        print("invalid choice!")
